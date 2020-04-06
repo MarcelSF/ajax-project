607b2cfaff79989debe4e3daa1c99546d14f93d2
Marcel Fonseca <marcelsf23br@gmail.com>
2020-04-06 16:37:20 -0300

    prepared for plugins lecture

--- src/index.js
+++ src/index.js
@@ -3,7 +3,17 @@
 // adf1f2d7
 // 48727053
 // 8691812a
-
+const insertMovies = (data) => {
+  const movies = document.querySelector('.movies');
+        movies.innerHTML = '';
+        data.Search.forEach((result) => {
+          const movie = `<li class="list-inline-item">
+              <img src="${result.Poster}" alt="">
+              <p>${result.Title}</p>
+            </li>`;
+          movies.insertAdjacentHTML("beforeend", movie)
+        });
+}
 // My API function
 const myApiFunction = () => {
   const input = document.querySelector('#input').value;
@@ -11,20 +21,7 @@ const myApiFunction = () => {
   console.log(request)
   fetch(request)
     .then(response => response.json())
-    .then((data) => {
-      const movies = document.querySelector('.movies');
-      movies.innerHTML = '';
-      data.Search.forEach((result) => {
-        const movie = `<li class="list-inline-item">
-            <img src="${result.Poster}" alt="">
-            <p>${result.Title}</p>
-          </li>`;
-        movies.insertAdjacentHTML("beforeend", movie)
-      })
-    });
-  //  then response
-  //  then data
-  //  then do something with the data
+    .then(insertMovies);
 }
 
 // #1 find the element
